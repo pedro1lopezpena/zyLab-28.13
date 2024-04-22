3ccacc4a71993fab521e7bd9eb4d92d4562caeff
Pinzonnn <167340781+Pinzonnn@users.noreply.github.com>
2024-04-22 11:16:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,32 +16,35 @@ int main(){
 
 	int item1_quantity;
 	int item2_quantity;
+//We ask for the name of Item 1
 
 	cout << "Item 1" << endl;
 	cout << "Enter the item name:" << endl;
 	getline(cin, item1_name);
 	item1.SetName(item1_name);
-
+//We enter the price to item 1 
 	cout << "Enter the item price:" << endl;
 	cin >> item1_price;
 	item1.SetPrice(item1_price);
-
+//We enter the item quantity for item 1
+	
 	cout << "Enter the item quantity:" << endl;
 	cin >> item1_quantity;
 	item1.SetQuantity(item1_quantity);
 
 	cout << endl;
 	cin.ignore();
-
+//We ask for the name of the item2
+	
 	cout << "Item 2" << endl;
 	cout << "Enter the item name:" << endl;
 	getline(cin, item2_name);
 	item2.SetName(item2_name);
-
+// We ask for the item2 price
 	cout << "Enter the item price:" << endl;
 	cin >> item2_price;
 	item2.SetPrice(item2_price);
-
+// We ask to input the item2 quantity
 	cout << "Enter the item quantity:" << endl;
 	cin >> item2_quantity;
 	item2.SetQuantity(item2_quantity);
